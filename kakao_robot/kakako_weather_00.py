a344330c8942f84e7227c470237b9bc592fafe2e
DESKTOP-4JASQU1\BLUE <damin321@ewhain.net>
2021-06-24 21:31:21 +0900

    kakao chatbot/ channel 접근 관련 파일
    크롬을 통해 날씨 검색, 스크린 캡쳐 및 저장, 카카오 채널 접근(로그인), 알림전송 및 날씨 이미지 전송
    시간 조정 함수 포함/ 테스트용 파일 포함

---
+++ kakao_robot/kakako_weather_00.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from datetime import datetime
+from PIL import Image
+import time
+import configparser
+import urllib
+
+
+ID = ''
+pw = ''
+
+KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
+ChatRoom = 'https://center-pf.kakao.com/_xfxcRGs/chats/4814011526591515'
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)
+
+driver.get(KaKaoURL)
+time.sleep(3)
+
+
+driver.find_element_by_id('loginEmail').send_keys(ID)
+driver.find_element_by_id('loginPw').send_keys(pw)
+driver.find_element_by_id('countryCodeRequired').find_element_by_xpath("//button[@type='submit']").click()
+time.sleep(3)
+
+iframes = driver.find_elements_by_tag_name('iframe')
+print('iframe=%d', len(iframes))
+
