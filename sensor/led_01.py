6fe207613f507fea39db8ec0d5a4f5c45c9cfc2a
DESKTOP-4JASQU1\BLUE <damin321@ewhain.net>
2021-06-24 21:34:03 +0900

    sensor 관련 파일
    led, motion, switch sensor 등을 통한 기본 코드 모음
    초인종 부분, led, motion센서를 이후 움직임 감지, 얼굴인식 부분과 결합에 사용

---
+++ sensor/led_01.py
@@ -0,0 +1,16 @@
+#led가 5초 간격으로 켜졌다 꺼졌다 실행
+#5번 반복하고 자동종료
+
+import RPi.GPIO as GPIO # GPIO를 이용하기 위한 라이브러리 불러오기
+from time import sleep # time 함수 사용을 위한 라이브러리 불러오기
+
+GPIO.setmode(GPIO.BCM) # 핀을 GPIO 핀 번호 기준으로 부름
+
+GPIO.setup(17, GPIO.OUT) # 핀의 모드를 설정합니다.
+
+for i in range(1, 5): # i를 1부터 5까지 1씩 올리며 아래 명령을 실행합니다.
+   GPIO.output(17, GPIO.HIGH) # 17번 핀을 HIGH상태로 설정합니다. LED가 켜집니다.
+   sleep(1) # 1초간 기다립니다.
+   GPIO.output(17, GPIO.LOW) # 17번 핀을 LOW 상태로 설정합니다. LED가 꺼집니다.
+   sleep(1)
+GPIO.cleanup() # GPIO 모듈의 리소스를 해제합니다.
