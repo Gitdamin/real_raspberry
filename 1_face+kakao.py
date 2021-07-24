ce388c9bf9848d46da4a460ac9f9a77ac7ba4c85
Gitdamin <86276347+Gitdamin@users.noreply.github.com>
2021-07-24 14:40:14 +0900

    Update 1_face+kakao.py
    1. 카카오 채널로 임의 저장 이미지 전송하기
    2. time out 시간 재설정
    3. 인식이 안되었을 경우 대체 이미지 조정
    -초인종을 누르자 마자 빠른 시간 내에 임의로 사진 저장->송출
    4. 설명 정리 및 추가

--- 1_face+kakao.py
+++ 1_face+kakao.py
@@ -1,8 +1,9 @@
 #초인종(스위치)을 누르면 LED ON, 사람의 눈 인식 시작
 #눈만 인식함으로써 마스크를 착용했을 때에도 인식
-#5장의 얼굴 캡쳐 & 저장 + 화면에 현재, 저장된 사진 함께 띄우기
-#화면 사이즈 변경-전체 화면으로 띄우기
-#time out 기능 (10sec, 15sec) & auto end
+#5장의 얼굴 캡쳐 및 저장 + 화면에 현재, 저장된 사진 함께 띄우기
+#if 눈 인식이 안될 시, 초인종 누른 순간 임의 캡쳐된 사진으로 대신 송출
+#화면 사이즈 변경->전체 화면으로 띄우기
+#time out 기능 (10sec) & auto end
 #카카오 챗봇 기능 추가 & 인터폰 기능 종료 후 카카오톡 접속 & '초인종 눌렀습니다' 출력
 
 
@@ -32,8 +33,8 @@ def set_size(img, scale):
     return cv2.resize(img, dsize=(int(img.shape[1]*scale), int(img.shape[0]*scale)), interpolation=cv2.INTER_AREA)
 
 #kakao_setting_user_version
-id = ''
-pw = ''
+id = ''  #개인 아이디
+pw = ''  #개인 비밀번호
 
 KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
 ChatRoom = 'https://center-pf.kakao.com/_xfxcRGs/chats/4814011526591515'
@@ -48,8 +49,9 @@ try: # 키보드 인터럽트 예외처리
         
           GPIO.output(17, GPIO.HIGH) # 17번 핀을 HIGH상태로 설정합니다. LED가 켜집니다.
           now = datetime.datetime.now()  #time out 기능
-          time_10 = now + datetime.timedelta(seconds =10)
-          time_15 = now + datetime.timedelta(seconds =15)
+          time_10 = now + datetime.timedelta(seconds =10)  #초인종을 누른 후 10초 지남
+          time_5 = now + datetime.timedelta(seconds =5)  #초인종을 누른 후 5초 지남
+          time_2 = now + datetime.timedelta(seconds =2)   #초인종을 누른 후 2초 지남
         
           #haarcascade_frontalface_default.xml 파일을 Classifier로 사용
           eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -59,33 +61,39 @@ try: # 키보드 인터럽트 예외처리
           while True:
              ret, img = cap.read() 
              gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-             eyes = eye_cascade.detectMultiScale(gray, scaleFactor= 1.5, minNeighbors=3, minSize=(10,10))
-               if len(eyes) :
-                  for  x, y, w, h in eyes :
-                      cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,255), 2, cv2.LINE_4)   #white mini box
-                      count += 1
-                      if count<6 :
+             now = datetime.datetime.now() 
+             if now < time_2 :
+                cv2.imwrite("/home/pi/Documents/face_detection/" + "test_capture_who.jpg", gray)  #초인종을 누른 직후 이미지 임의 캡쳐 및 저장
+                
+             eyes = eye_cascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors=3, minSize=(10,10))
+             if len(eyes) :  #사람의 눈 인식
+                 for  x, y, w, h in eyes :
+                     cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,255), 2, cv2.LINE_4)   #white mini box
+                     count += 1
+                     if count<6 :  #5장의 이미지 캡쳐 및 저장
                          cv2.imwrite("/home/pi/Documents/face_detection/" + "test_capture_0" + str(count) + ".jpg", gray) 
             
-             big_size = set_size(img, 2.5)    #size bigger
-             cv2.imshow("big_size", big_size)     #size up 영상 출력
-             if count>3 :
-                 img2 = cv2.imread('/home/pi/Documents/face_detection/test_capture_03.jpg', 1)
-                 cv2.imshow('Captured Image', img2)
+             big_size = set_size(img, 2.5)    
+             cv2.imshow("big_size", big_size)   #size up 영상 출력
+            
+             if count < 3 :     #얼굴인식이 안되었을 경우, 초인종 누른 직후 임의 캡쳐한 이미지를 대신 송출
+                img3 = cv2.imread('/home/pi/Documents/face_detection/test_capture_who.jpg', 1) 
+                cv2.imshow('Captured Image', img3)
+                
+             else :     #얼굴인식 및 이미지 저장이 잘 되었을 경우
+                img2 = cv2.imread('/home/pi/Documents/face_detection/test_capture_03.jpg', 1)  
+                cv2.imshow('Recognized Image', img2)
+                
              k = cv2.waitKey(30) & 0xff
              if k == 27: # press 'ESC' to quit # ESC를 누르면 종료
                 break
-             elif count >= 5 :
-                 GPIO.output(17, GPIO.LOW) # 17번 핀을 LOW 상태로 설정합니다. LED가 꺼집니다.
-                 now = datetime.datetime.now()
-                 if now >= time_10 :
-                     break
+                
              now = datetime.datetime.now()          
-             if now >= time_15 :
-                if count <1 :
-                    print('no detected..')
-                    GPIO.output(17, GPIO.LOW) # 17번 핀을 LOW 상태로 설정합니다. LED가 꺼집니다.
-                    break
+             if now >= time_10 :  #time out
+                GPIO.output(17, GPIO.LOW) # 17번 핀을 LOW 상태로 설정합니다. LED가 꺼집니다.
+                if count < 3 :
+                    print("no detected") 
+                break
              
           cap.release()
           cv2.destroyAllWindows()
@@ -111,12 +119,14 @@ try: # 키보드 인터럽트 예외처리
         
           #글 작성
           driver.find_element_by_id('chatWrite').send_keys('초인종을 눌렀습니다.')  #메시지 작성
-          driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click()  #전송버튼
-          #//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button
-          sleep(3)
+          if count > 3 : #얼굴인식 및 이미지 저장이 잘 되었을 경우
+             driver.find_element_by_xpath("//input[@class='custom uploadInput']").send_keys('/home/pi/Documents/face_detection/test_capture_03.jpg') 
+          else :  #얼굴인식이 안되었을 경우, 초인종 누른 직후 임의 캡쳐한 이미지를 대신 전달
+              driver.find_element_by_xpath("//input[@class='custom uploadInput']").send_keys('/home/pi/Documents/face_detection/test_capture_who.jpg')
+          
           driver.quit()
           sleep(1)
-          exit()  #프로그램 종료 (임의)
+          exit()  #프로그램 종료
    
 
        else:
