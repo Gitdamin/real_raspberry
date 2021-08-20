3799e52f7be5015dcb6391bbaebad2eb2ee6c644
Damin <86276347+Gitdamin@users.noreply.github.com>
2021-08-20 18:55:49 +0900

    Update 1_face_main.py

--- 1_face_main.py
+++ 1_face_main.py
@@ -1,59 +1,62 @@
-## face recognization + capture 5 images + show together 
-## 학습된 이미지와 비교분석하여 외부인의 신원 및 정보를 전달
+## Main code [eye detection + recognize each person + send the kakao talk message to user]
+# [Monitor] show both current video in front of the door and captured image when you push the button
+# [Phone] send the info such as captured image and one's id to user  
 
-import RPi.GPIO as GPIO # GPIO를 이용하기 위한 라이브러리 불러오기
-from time import sleep # time 함수 사용을 위한 라이브러리 불러오기
+import RPi.GPIO as GPIO 
+from time import sleep 
 import datetime
 from selenium import webdriver
 import urllib
 
-#GPIO 핀 번호 setting
-GPIO.setmode(GPIO.BCM) # 핀을 GPIO 핀 번호 기준으로 설정
+#GPIO pin num setting
+GPIO.setmode(GPIO.BCM) 
 GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # pull down mode
-GPIO.setup(17, GPIO.OUT) #  led
+GPIO.setup(17, GPIO.OUT) # led setup
 
-import numpy as np #얼굴 인식 후 박스로 표시 
+import numpy as np 
 import cv2
 
-# 크기 변경 함수
+# change to a larger size
 def set_size(img, scale):
     return cv2.resize(img, dsize=(int(img.shape[1]*scale), int(img.shape[0]*scale)), interpolation=cv2.INTER_AREA)
 
 def kakao():
     
     #kakao setting
-    id = ''  #개인 아이디
-    pw = ''  #개인 비밀번호
+    
+    id = ''  # personal info
+    pw = '' 
 
     KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
     ChatRoom = 'https://center-pf.kakao.com/_xfxcRGs/chats/4814011526591515'
     options = webdriver.ChromeOptions()
 
-    #크롬 드라이버 로드
+    # Chrome driver load
     driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)
     driver.implicitly_wait(3)
     
-    #user-agent 변경
+    # change user-agent
     options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187")  
           
     driver.get(KaKaoURL)
     sleep(1)
           
-    driver.find_element_by_id('id_email_2').send_keys(id)  #아이디 작성
-    driver.find_element_by_id('id_password_3').send_keys(pw)  #비밀번호 작성
-    driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()  #입력버튼 클릭
+    driver.find_element_by_id('id_email_2').send_keys(id)  # write id
+    driver.find_element_by_id('id_password_3').send_keys(pw)  # write password
+    driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()  # click the input button
     sleep(1)
         
-    #채팅방 로드
+    # personal chatroom load
     driver.get(ChatRoom)
     sleep(1)
         
-    #글 작성
-    driver.find_element_by_id('chatWrite').send_keys('초인종을 눌렀습니다.')  #메시지 작성
-    if count > 3 :   #얼굴인식 및 이미지 저장이 잘 되었을 경우
+    # write message
+    driver.find_element_by_id('chatWrite').send_keys('초인종을 눌렀습니다.')  
+    # When eye recognition and image storage are successful
+    if count > 3 :   
           driver.find_element_by_xpath("//input[@class='custom uploadInput']").send_keys('/home/pi/Documents/face_detection/test_capture_03.jpg') 
             
-          #학습시킨 이미지 불러오기 
+          # Trained code load
           recognizer = cv2.face.LBPHFaceRecognizer_create()
           recognizer.read('/home/pi/Documents/face_detection/trainer/trainer.yml')
         
@@ -61,45 +64,49 @@ def kakao():
           #cv2.imread('/home/pi/Documents/face_detection/test_capture_03.jpg')
           gray_2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-          #값 받아오기 
+          # Get values
           id, confidence = recognizer.predict(gray_2)
           print(id)
         
-          #값 반대로 생각하기 
+          # match the name
           id = id-1
           id = names[id]
-          if confidence > 20 :
+            
+          # inverse 
+          confidence = 100 - confidence
+          if confidence < 65 :
                 id = "unknown"
           
-          #confidence = "  {0}%".format(round(100 - confidence))
-            
-          #글 작성                 
-          driver.find_element_by_id('chatWrite').send_keys('외부인의 신원(id) = ' + str(id))  #메시지 작성
-          driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click()  #전송버튼
+         
+          # write message             
+          driver.find_element_by_id('chatWrite').send_keys('외부인의 신원(id) = ' + str(id)) 
+          driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click() 
         
-        
-    else :    #얼굴인식이 안되었을 경우, 초인종 누른 직후 임의 캡쳐한 이미지를 대신 전달
+    # When eye recognition failed
+    # send an alternative image that is saved immediately after press the doorbell
+    else :   
           driver.find_element_by_xpath("//input[@class='custom uploadInput']").send_keys('/home/pi/Documents/face_detection/test_capture_who.jpg')
           
     driver.quit()
     sleep(1)
-    exit()  #프로그램 종료
+    exit()  # End the program
    
 
-try: # 키보드 인터럽트 예외처리
+try: 
     count = 0  #initialize
     while 1 :
        sleep(0.5)
        if GPIO.input(14) is 0:
           print('PUSH THE BUTTON')
         
-          GPIO.output(17, GPIO.HIGH) # 17번 핀을 HIGH상태로 설정합니다. LED가 켜집니다.
+          GPIO.output(17, GPIO.HIGH) # 17 pin High -> LED on
           now = datetime.datetime.now()  #time out 기능
-          time_10 = now + datetime.timedelta(seconds =10)  #초인종을 누른 후 10초 지남
-          time_5 = now + datetime.timedelta(seconds =5)  #초인종을 누른 후 5초 지남
-          time_2 = now + datetime.timedelta(seconds =2)   #초인종을 누른 후 2초 지남
+          # N seconds after ringing the doorbell
+          time_10 = now + datetime.timedelta(seconds =10) 
+          time_5 = now + datetime.timedelta(seconds =5)  
+          time_2 = now + datetime.timedelta(seconds =2)  
         
-          #haarcascade_frontalface_default.xml 파일을 Classifier로 사용
+          # Use haarcascade_frontalface_default.xml file as Classifier
           eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
           cap = cv2.VideoCapture(-1)
           cap.set(3,640) # set Width
@@ -108,39 +115,43 @@ try: # 키보드 인터럽트 예외처리
              ret, img = cap.read() 
              gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
              now = datetime.datetime.now() 
-             if now < time_2 :  #초인종을 누른 직후 이미지 임의 캡쳐 및 저장
+             # capture video and save images immediately after pressing the doorbell
+             if now < time_2 :  
                 cv2.imwrite("/home/pi/Documents/face_detection/" + "test_capture_who.jpg", gray)  
                 
              eyes = eye_cascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors=10, minSize=(15,15))
-             if len(eyes) :  #사람의 눈 인식
+             if len(eyes) :  # eye detection
                  for  x, y, w, h in eyes :
-                     cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,255), 2, cv2.LINE_4)   #white mini box
+                     cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,255), 2, cv2.LINE_4)   # show white mini box on img
                      count += 1
-                     if count<6 :  #5장의 이미지 캡쳐 및 저장
+                     if count<6 :  # capture and save 5 images
                          cv2.imwrite("/home/pi/Documents/face_detection/" + "test_capture_0" + str(count) + ".jpg", gray) 
                         
-             #size up 영상 출력
+             # show size-up img on monitor
              big_size = set_size(img, 2.5)    
              cv2.imshow("big_size", big_size)   
              
-             if count < 3 :  #얼굴인식이 안되었을 때, 초인종 누른 직후 임의 저장된 이미지 대신 송출
+             # When eye recognization failed
+             # send an alternative image that is saved immediately after press the doorbell
+             if count < 3 : 
                 img_who = cv2.imread('/home/pi/Documents/face_detection/test_capture_who.jpg', 1)
                 cv2.imshow('Captured Image', img_who)
-             
-             else :  #얼굴인식이 성공적으로 진행
-                cv2.destroyWindow("Captured Image")  #이전의 창 제거 후 정확한 사진 송출
+                
+             # When eye recognition is successful
+             else :  
+                cv2.destroyWindow("Captured Image")  # removing the previous window
                 img_cap = cv2.imread('/home/pi/Documents/face_detection/test_capture_03.jpg', 1)
-                cv2.imshow('Recognized Image', img_cap)
+                cv2.imshow('Recognized Image', img_cap) # show accurate image
   
                 
              k = cv2.waitKey(30) & 0xff
-             if k == 27: # press 'ESC' to quit # ESC를 누르면 종료
+             if k == 27: # press 'ESC' to quit 
                 break
                 
-             #time out   
+             # time out   
              now = datetime.datetime.now()          
              if now >= time_10 :  
-                GPIO.output(17, GPIO.LOW) # 17번 핀을 LOW 상태로 설정합니다. LED가 꺼집니다.
+                GPIO.output(17, GPIO.LOW) # 17 pin Low -> LED off
                 if count < 3 :
                     print("no detected") 
                 break
