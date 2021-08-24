a5e57c060faf94ff6d8bf5ffc16be1468ce2096f
git-songsh <86220586+git-songsh@users.noreply.github.com>
2021-08-24 17:36:43 +0900

    Update 2_ultra+detect+live+kakao.py

--- 2_ultra+detect+live+kakao.py
+++ 2_ultra+detect+live+kakao.py
@@ -2,10 +2,9 @@ from selenium import webdriver
 import RPi.GPIO as GPIO # GPIO를 이용하기 위한 라이브러리 불러오기
 import time # time 함수 사용을 위한 라이브러리 불러오기
 import datetime
+
 import app # 실시간 영상 스트리밍 관련 app.py 모듈 
 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import cv2
 
 GPIO.setmode(GPIO.BCM) # 핀을 GPIO 핀 번호 기준으로 부름
@@ -57,57 +56,6 @@ def measure_average(): # 10초동안 평균 거리 측정하는 함수
     
     print(str(distance))    
     return distance
-
-"""def record(): # 파이카메라로 영상 녹화하는 함수
-  with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    now = datetime.datetime.now()
-    filename = now.strftime('%Y-%m-%d %H:%M:%S')
-    camera.start_recording(output = filename + '.h264') #h.264 코덱
-    camera.wait_recording(5)
-    camera.stop_recording()
-    """
-
-def detect(img, cascade): # fullbody 인식하는 함수
-    rects = fullbody_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30))
-                                            #  flags=cv2.CASCADE_SCALE_IMAGE
-    if len(rects) == 0:
-        return []
-    rects[:,2:] += rects[:,:2]
-    return rects
-
-def draw_rects(img, rects, color): # 인식한 부분 사각형 표시하는 함수
-    for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
-        
-def login(): # 카카오톡 챗봇으로 로그인 하는 함수
-    id = '~' # 카카오톡 아이디
-    pw = '~' # 카카오톡 비밀번호
-
-    KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
-    ChatRoom = 'https://center-pf.kakao.com/_dLeCs/chats/4812080438129747' # 카카오톡챗봇 주소
-    options = webdriver.ChromeOptions()
-
-    # user-agent 변경
-    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187")
-
-    # 크롬 드라이버 로드
-    driver = webdriver.Chrome('/lib/chromium-browser/chromedriver', options=options)
-    driver.implicitly_wait(3)
-
-    # 카카오 메인 페이지 로드
-    driver.get(KaKaoURL)
-    time.sleep(3)
-
-    driver.find_element_by_id('id_email_2').send_keys(id)
-    driver.find_element_by_id('id_password_3').send_keys(pw)
-
-    driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
-    time.sleep(3)
-
-    # 채팅방 로드
-    driver.get(ChatRoom)
-    time.sleep(3)
 '''
 try:  
     # 카메라 초기설정
@@ -204,7 +152,34 @@ try:
                   # record()
                     
 
-                    login()
+     
+                    id = '~' # 카카오톡 아이디
+                    pw = '~' # 카카오톡 비밀번호
+
+                    KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
+                    ChatRoom = 'https://center-pf.kakao.com/_dLeCs/chats/4812080438129747' # 카카오톡챗봇 주소
+                    options = webdriver.ChromeOptions()
+
+                    # user-agent 변경
+                    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187")
+
+                    # 크롬 드라이버 로드
+                    driver = webdriver.Chrome('/lib/chromium-browser/chromedriver', options=options)
+                    driver.implicitly_wait(3)
+
+                    # 카카오 메인 페이지 로드
+                    driver.get(KaKaoURL)
+                    time.sleep(3)
+
+                    driver.find_element_by_id('id_email_2').send_keys(id)
+                    driver.find_element_by_id('id_password_3').send_keys(pw)
+
+                    driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
+                    time.sleep(3)
+
+                    # 채팅방 로드
+                    driver.get(ChatRoom)
+                    time.sleep(3)
                     driver.find_element_by_id('chatWrite').send_keys('움직임이 감지되었습니다.') # 메세지 작성
                     time.sleep(3)
                     driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click() # 메세지 전송 버튼
@@ -214,10 +189,10 @@ try:
                     driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click() # 메세지 전송 버튼
                     time.sleep(5)
 
-               """  driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/button').click() # 파일 업로드 버튼
+                    driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/button').click() # 파일 업로드 버튼
                     driver.find_element_by_css_selector('#kakaoWrap > div.chat_popup > div.popup_body > div > div.write_chat2 > div.write_menu > div:nth-child(1) > div.upload_btn > input').send_keys('파일경로')
                     # 짧은 영상 파일 전송 
-                    time.sleep(20) """
+                    time.sleep(20) 
 
                     driver.quit()
                     time.sleep(60)
