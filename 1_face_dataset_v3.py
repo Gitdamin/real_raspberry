541b10c8bcfb6a5d280a79541ef95effef3e7bc8
Damin <86276347+Gitdamin@users.noreply.github.com>
2021-09-02 17:56:16 +0900

    Update 1_face_dataset_v3.py

--- 1_face_dataset_v3.py
+++ 1_face_dataset_v3.py
@@ -1,19 +1,23 @@
-# Control brightness - add/ sub
+## Data set code ##
+# Control image brightness - add/ sub
+# special case2 #
 
 import cv2
 import numpy as np
 
-src = cv2.imread('/home/pi/Documents/face_detection/mask_success.jpg', cv2.IMREAD_COLOR)
-#num = 1
+# call image you want to change
+src = cv2.imread('/home/pi/Documents/face_detection/~.jpg', cv2.IMREAD_COLOR)
 val = 20
 count = 0
+# input personal(outsider) id
 face_id = input('\n write your id and push the enter button => ')
-char = 'mask'
+
+# Inform image characteristic
+char = 'mask'  # EX) 'glasses1' ,'glasses2'
 while 1 :
     
     #src = cv2.imread('/home/pi/Documents/face_detection/image' + str(num) + '.jpg', cv2.IMREAD_COLOR)
     array = np.full(src.shape, (val, val, val), dtype=np.uint8)
-    #num += 1
     add = cv2.add(src, array)
     sub = cv2.subtract(src, array)
     val += 10
@@ -22,7 +26,8 @@ while 1 :
         
         cv2.imwrite("dataset/User." + str(face_id) + '.' + str(char) + '_add' + '.' + str(count) + ".jpg", add)
         cv2.imwrite("dataset/User." + str(face_id) + '.' + str(char) + '_sub' + '.' + str(count) + ".jpg", sub)
-    if val > 100 :
+    
+    if val > 80 :
         break
     
     
